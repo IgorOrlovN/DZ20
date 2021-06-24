7c5ae9e9729f4c2d9ac297eabf89832bacba9cdc
OrlovIgorN <orlikman@yandex.ru>
2021-06-24 21:00:38 +0300

    7

---
+++ Source/SnakeGame/interactable.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "interactable.h"
+
+// Add default functionality here for any Iinteractable functions that are not pure virtual.
+void Iinteractable::Interact(AActor* Interactor, bool bIsHead)
+{
+
+}
